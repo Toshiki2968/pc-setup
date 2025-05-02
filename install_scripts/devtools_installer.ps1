cb21f27ae5aed4f5685b424f79c16ef9eea7c9a5
t-sumiya-uf <tsk2968@gmail.com>
2025-05-02 21:20:37 +0900

    visual studioの指定方法変更

--- install_scripts/devtools_installer.ps1
+++ install_scripts/devtools_installer.ps1
@@ -1,1 +1,1 @@
-choco install -y git vscode docker-desktop nodejs dotnet-sdk dbeaver community.visualstudio
+choco install -y git vscode docker-desktop nodejs dotnet-sdk dbeaver visualstudio2022community
